ee97ddea606cc9117714ffa5e98aed605f000517
Mohammad Issawi <2000issawi@gmail.com>
2022-11-27 19:40:19 +0300

    Fix interactive mode faulty bug

--- game/game.go
+++ game/game.go
@@ -7,29 +7,33 @@ import (
 )
 
 type game struct {
-	algor      algor.Algor
-	puzzlePath string
-	heuristicFunc func(*state.State)*int
+	algor         algor.Algor
+	puzzlePath    string
+	heuristicFunc func(*state.State) *int
 }
 
-func NewGame(puzzle string, algor algor.Algor,heuristicFunc func(*state.State) *int) game {
+func NewGame(puzzle string, algor algor.Algor, heuristicFunc func(*state.State) *int) game {
 	var g game
 	g.puzzlePath = puzzle
 	g.algor = algor
-	g.heuristicFunc=heuristicFunc
+	g.heuristicFunc = heuristicFunc
 	return g
 }
 
 func (g *game) Play() {
-	start := state.NewState(g.puzzlePath,g.heuristicFunc)
+	start := state.NewState(g.puzzlePath, g.heuristicFunc)
 	final := g.algor.Search(start)
 	if final != nil {
-		path := final.Path()
-		for _, s := range path {
-			println(s.String())
+		if _, ok := g.algor.(*algor.NoAlgor); ok {
+			println(final.String())
+		} else {
+			path := final.Path()
+			for _, s := range path {
+				println(s.String())
+			}
 		}
 		println("YAY")
-		fmt.Printf("Moves = %d",final.Moves)
+		fmt.Printf("Moves = %d", final.Moves)
 	} else {
 		print("No solution was found")
 	}
