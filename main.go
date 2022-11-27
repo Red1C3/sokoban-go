c252773e373f68ee9ae05d6cecfb78d8b09b4bc5
Mohammad Issawi <2000issawi@gmail.com>
2022-11-27 22:21:44 +0300

    Enable cmd line args for algorithms and puzzle paths

--- main.go
+++ main.go
@@ -3,15 +3,76 @@ sokoban-go is an automated sokoban puzzles solver.
 
 It provides easy to extend structures for implementing better searching algorithms
 without having to design the whole program.
+
+Usage:
+
+	sokoban-go [algorithm] [puzzlepath]
+
+algorithm options are:
+
+	human
+	dfs
+	bfs
+	astar-gh2
+	astar-gh1
+	astar-h1
+	astar-h2
+	hc-gh2
+	hc-gh1
+	hc-h2
+	hc-h1
+
+puzzlepath should be a suitable 2D JSON string array,
+see "puzzles" folder for examples and [state/consts] for possible tiles characters.
 */
 package main
 
 import (
+	"log"
+	"os"
 	"sokoban/algor"
 	"sokoban/game"
 )
 
 func main() {
-	g := game.NewGame("./puzzles/simple.json", &algor.Bfs{}, algor.GH2) //TODO take terminal args instead of hardcode
-	g.Play()
+	if len(os.Args) < 3 {
+		log.Fatal("No enough command line arguments provided, please rtfm.")
+	}
+	switch os.Args[1] {
+	case "dfs":
+		g := game.NewGame(os.Args[2], &algor.Dfs{}, algor.NoFunc)
+		g.Play()
+	case "bfs":
+		g := game.NewGame(os.Args[2], &algor.Bfs{}, algor.NoFunc)
+		g.Play()
+	case "astar-gh2":
+		g := game.NewGame(os.Args[2], &algor.AStar{}, algor.GH2)
+		g.Play()
+	case "astar-gh1":
+		g := game.NewGame(os.Args[2], &algor.AStar{}, algor.GH1)
+		g.Play()
+	case "astar-h2":
+		g := game.NewGame(os.Args[2], &algor.AStar{}, algor.H2)
+		g.Play()
+	case "astar-h1":
+		g := game.NewGame(os.Args[2], &algor.AStar{}, algor.H1)
+		g.Play()
+	case "hc-gh2":
+		g := game.NewGame(os.Args[2], &algor.HillClimbing{}, algor.GH2)
+		g.Play()
+	case "hc-gh1":
+		g := game.NewGame(os.Args[2], &algor.HillClimbing{}, algor.GH1)
+		g.Play()
+	case "hc-h2":
+		g := game.NewGame(os.Args[2], &algor.HillClimbing{}, algor.H2)
+		g.Play()
+	case "hc-h1":
+		g := game.NewGame(os.Args[2], &algor.HillClimbing{}, algor.H1)
+		g.Play()
+	case "human":
+		g := game.NewGame(os.Args[2], &algor.NoAlgor{}, algor.NoFunc)
+		g.Play()
+	default:
+		log.Fatalf("Unknown algorithm %s", os.Args[1])
+	}
 }
